be0ea5756a986defb1272824afa53cbebb26d7b3
Rolly Padua <32024336+Vandolph703@users.noreply.github.com>
2017-11-20 08:08:03 -0800

    Add files via upload

---
+++ app/src/main/java/cse120project/catplan/Days.java
@@ -0,0 +1,23 @@
+package com.example.matri.catplan;
+
+/**
+ * Created by Rolly-kun on 11/17/2017.
+ */
+
+/*
+ * Intention of this class is to be used with catalog, courses, and scheduler. Simple and easy way
+ * to retrieve the day of the week
+ */
+public class Days {
+    public enum Day{
+        SUN,
+        MON,
+        TUE,
+        WED,
+        THUR,
+        FRI,
+        SAT;
+
+
+    }
+}
