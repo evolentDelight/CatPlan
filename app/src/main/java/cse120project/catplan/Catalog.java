96aaeaf95c569364a71e5f246b86b94592435175
Rolly Padua <32024336+Vandolph703@users.noreply.github.com>
2017-11-20 05:02:09 -0800

    Add files via upload

--- app/src/main/java/cse120project/catplan/Catalog.java
+++ app/src/main/java/cse120project/catplan/Catalog.java
@@ -1,4 +1,4 @@
-package cse120project.catplan;
+package com.example.matri.catplan;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -20,8 +20,8 @@ public class Catalog extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Snackbar.make(view, "View Catalog", Snackbar.LENGTH_LONG)
+                        .setAction("Viewing Catalog", null).show();
             }
         });
     }
